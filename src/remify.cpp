aac0479b875cc8436919e7e667179370bf9a45b9
Giuseppe Arena <g.arena@tilburguniversity.edu>
2022-02-17 11:11:35 +0100

    remify.h added. It contains functions that might be used from other packages in the remverse ensemble

--- src/remify.cpp
+++ src/remify.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <algorithm> 
 #include "messages.h"
+#include "remify.h"
 
 
 // @title rearrangeDataFrame
@@ -180,47 +181,6 @@ Rcpp::List getIntereventTime(Rcpp::RObject time,
     return out;
 }
 
-// @title getDyadIndex
-//
-// @param actor1 id of actor1 from 0 to N-1
-// @param actor2 id of actor2 from 0 to N-1
-// @param type id of event type from 0 to C-1
-// @param N number of actors
-// @param directed bool FALSE/TRUE if the networks is directed (TRUE) or not (FALSE)
-//
-// @return dyad index according to the combination of id's of actor1/actor2/type
-int getDyadIndex(double actor1, double actor2, double type, int N, bool directed) {
-
-    int dyad = -999; // returning impossible index if the dyad is a self-edge (i.e., sender and receiver are the same actor)
-    if(actor1 != actor2){
-        if(!directed){ // when directed == FALSE we sort actor1 and actor2
-            if(actor1 < actor2){
-                double dyad_loc = (N*(N-1)/2)*type+(N-1)*actor1+actor2-actor1-1-(actor1*actor1)/2;
-                if(actor1>0){
-                    dyad_loc += actor1/2;
-                }
-                dyad = dyad_loc;
-            }
-            else{
-                double dyad_loc = (N*(N-1)/2)*type+(N-1)*actor2+actor1-actor2-1-(actor2*actor2)/2;
-                if(actor2>0){
-                    dyad_loc += actor2/2;
-                }
-                dyad = dyad_loc;
-            }
-        }
-        else{ 
-            // when directed == TRUE (we do not sort) (actor1 = sender, actor2 = receiver)
-            double dyad_loc = N*(N-1)*type+(N-1)*actor1+actor2;
-            if(actor2>actor1){
-                dyad_loc -= 1;
-            }
-            dyad = dyad_loc;
-        }
-    }
-
-    return dyad;
-}
 
 // @title getRisksetSender
 //
